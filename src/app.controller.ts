a7a880a46c81641c31386ea92d80a16c753c27a0
dongyuekai <457545648@qq.com>
2023-08-14 14:00:55 +0800

    feat: 更新

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// controllers是控制器 只能被注入
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+// service可以被注入也可以注入到别的对象 所以用 @injectable声明
 @Injectable()
 export class AppService {
   getHello(): string {
