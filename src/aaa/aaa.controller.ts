95f851856296c0c7337e91b016c01dd97dd69027
dongyuekai <457545648@qq.com>
2023-08-17 14:47:07 +0800

    feat： Guard路由守卫

--- src/aaa/aaa.controller.ts
+++ src/aaa/aaa.controller.ts
@@ -1,9 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  CanActivate,
+  Injectable,
+  ExecutionContext,
+  UseGuards,
+} from '@nestjs/common';
 import { AaaService } from './aaa.service';
 import { CreateAaaDto } from './dto/create-aaa.dto';
 import { UpdateAaaDto } from './dto/update-aaa.dto';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    return true;
+  }
+}
 
 @Controller('aaa')
+@UseGuards(RolesGuard) // Guard 是路由守卫的意思，可以用于在调用某个 Controller 之前判断权限，返回 true 或者 false 来决定是否放行
 export class AaaController {
   constructor(private readonly aaaService: AaaService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RolesGuard } from './aaa/aaa.controller';
 
 // 入口模块
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalGuards(new RolesGuard());
   await app.listen(3000);
 }
 bootstrap();
